e99c0e0850c963b7162ec24a2d2bdf9cf9c412c5
stormlin <376997313@qq.com>
2017-08-09 13:52:11 +0800

    Imported from storm.js

--- js/storm.js
+++ js/storm.js
@@ -9,14 +9,44 @@
  *
  */
 
-//Default settings
+/**
+ * Default settings for this project. All relative path is from the side of root.
+ *
+ * @type {{categoriesPath: string, indexPath: string, postPath: string, title: string, motto: string}}
+ */
 var settings = {
     categoriesPath: "./conf/categories.xml",
     indexPath: "./md/index.md",
     postPath: "./conf/posts.xml",
     title: "stormlin",
-    motto: "Yet another full stack developer."
+    motto: "On the way to be perfect"
 };
+
+/**
+ * Page names and URLs for links in navigation bar.
+ *
+ * @type {Array}
+ */
+var navPages = [];
+navPages = [
+    index = {
+        url: "./index.html",
+        name: "首页"
+    },
+    category = {
+        url: "./category.html",
+        name: "分类"
+    },
+    about = {
+        url: "./page.html?name=about",
+        name: "关于我"
+    },
+    dogFood = {
+        url: "http://u4813096.viewer.maka.im/k/OQHMX6E6?from=timeline",
+        name: "狗粮合集"
+    }
+];
+
 
 /**
  * Get the target parameter specified by name from url.
@@ -25,9 +55,12 @@ var settings = {
  * @returns {null}
  */
 function getParaValue(name) {
+
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var result = window.location.search.substr(1).match(reg);
+
     if (result !== null) return result[2];
+
     return null;
 }
 
@@ -37,44 +70,31 @@ function getParaValue(name) {
  * @constructor
  */
 function RedirectTo404() {
+
     window.location = "./404.html";
+
 }
 
 /**
- * Get the blog configs.
- *
- * @param xmlFilePath File path to xml config file.
+ * Get the blog poster.
  */
-function getBlogConfig(xmlFilePath) {
-    $.ajax({
-        url: xmlFilePath,
-        dataType: "xml",
-        type: "GET",
-        error: function () {
-            alert("Unable to reach the config file.");
-        },
-        success: function (xml) {
-            $(xml).find("config").each(function () {
-                var title = "<h1>" + $(this).children("title").text() + "</h1>";
-                var motto = "<p>" + $(this).children("motto").text() + "</p>";
-                $(".markdown-body").append(title + motto);
-                //note: The usage of find() and each().
-//                var item = $(this).find("category").find("category-item").children("item-text").text();
-//                alert(item + i);
-            })
-        }
-    });
+function getBlogPoster() {
+
+    var poster = '<h1>' + settings.title + '</h1>';
+    poster += '<h2>' + settings.motto + '</h2>';
+    $("#blog-info").append(poster);
+
 }
 
 /**
  * Return the text for target category name
  *
  * @param categoryName Require category
- * @returns text Category-Text
+ * @returns string Category-Text
  */
 function getCategoryTextByName(categoryName) {
 
-    var text;
+    var text = "";
 
     $.ajax({
         url: settings.categoriesPath,
@@ -86,9 +106,9 @@ function getCategoryTextByName(categoryName) {
         },
         success: function (xml) {
             $(xml).find("categories").find("category-item").each(function () {
-                var item = $(this).children("item-id").text();
+                var item = $(this).children("name").text();
                 if (item === categoryName) {
-                    text = $(this).children("item-text").text();
+                    text = $(this).children("text").text();
                     return text;
                 }
             })
@@ -102,7 +122,8 @@ function getCategoryTextByName(categoryName) {
  * Get categories array from categories.xml
  */
 function getCategories() {
-    //todo: Add categories at the head of each page.
+
+    var categories = [];
 
     $.ajax({
         url: settings.categoriesPath,
@@ -112,22 +133,27 @@ function getCategories() {
         },
         success: function (xml) {
             $(xml).find("categories").find("category-item").each(function () {
-                var itemText = $(this).children("item-text").text();
-                var itemName = $(this).children("item-id").text();
-                //console.log(itemText.text());
-                //note: The usage of js array
-                //categories.push(itemText.text());
-                $(".nav-list").append('<li class="nav-list-item"><a href="./category.html?category=' + itemName + '">' + itemText + '</a></li>');
+                var category = {
+                    name: $(this).children("name").text(),
+                    text: $(this).children("text").text()
+                };
+                categories.push(category);
             })
         }
     });
+
+    categories.sort(by("name"));
+    categories.reverse();
+
+    return categories;
 
 }
 
 /**
  * Print the post list stored in "index.md".
  */
-function getPostList() {
+function getIndexPostList() {
+
     var requestedPost = [];
 
     $.ajax({
@@ -152,60 +178,14 @@ function getPostList() {
         }
     });
 
+    requestedPost.sort(by("date"));
+    requestedPost.reverse();
+
     return requestedPost;
 }
 
-function getHeader() {
-
-    var xmlFilePath = settings.xmlFilePath;
-    var indexFilePath = settings.indexPath;
-
-    $.ajax({
-        url: xmlFilePath,
-        dataType: "xml",
-        async: true,
-        type: "GET",
-        error: function () {
-            alert("Unable to reach the config file.");
-        },
-        success: function (xml) {
-            $(xml).find("config").each(function () {
-                var title = "<h1>" + $(this).children("title").text() + "</h1>";
-                var motto = "<p>" + $(this).children("motto").text() + "</p>";
-                $("#title").append(title + motto);
-                //note: The usage of find() and each().
-//                var item = $(this).find("category").find("category-item").children("item-text").text();
-//                alert(item + i);
-            })
-        }
-    });
-
-    //var categories = new Array();
-
-    //note: The execute sequence of js
-    $.ajax({
-        url: xmlFilePath,
-        async: true,
-        error: function () {
-            alert("Unable to reach the xml config file.");
-        },
-        success: function (xml) {
-            $(xml).find("category").find("category-item").each(function () {
-                var itemText = $(this).children("item-text");
-                //console.log(itemText.text());
-                //note: The usage of js array
-                //categories.push(itemText.text());
-                $("#categories").append("<li class='categories-item'><a href='#'>" + itemText.text() + "</a></li>");
-            });
-        }
-    });
-
-}
-
-//todo: 获取符合目的 category 的所有文章的链接
-//note: 不要使用在 xml 文件中用 ------- 来做表格
 /**
- * Get all requested post links
+ * Get all requested post links.
  *
  * @param categoryName Target category
  */
@@ -238,6 +218,48 @@ function getPostListWithCategory(categoryName) {
         }
     });
 
+    requestedPost.sort(by("date"));
+    requestedPost.reverse();
+
+    return requestedPost;
+
+}
+
+/**
+ * Get post object
+ *
+ * @param postName Post name
+ * @returns {*} Target post
+ */
+function getPostByName(postName) {
+
+    var requestedPost = null;
+
+    $.ajax({
+        url: settings.postPath,
+        async: false,
+        error: function (e) {
+            alert("Unable to reach the post list file.");
+            console.log(e.message);
+        },
+        success: function (xml) {
+            $(xml).find("posts").find("post-item").each(function () {
+                var post = {
+                    id: $(this).children("id").text(),
+                    name: $(this).children("name").text(),
+                    title: $(this).children("title").text(),
+                    category: $(this).children("category").text(),
+                    date: $(this).children("date").text(),
+                    description: $(this).children("description").text()
+                };
+
+                if (post.name === postName) {
+                    requestedPost = post;
+                }
+            })
+        }
+    });
+
     return requestedPost;
 
 }
@@ -253,22 +275,189 @@ function printPostList(posts) {
         new RedirectTo404();
     } else {
         for (var i = 0; i < posts.length; i++) {
-            var category = '<a href="./category.html?category=' + posts[i].category + '">' + getCategoryTextByName(posts[i].category) + '</a>';
-            var item = '<div class="post-item"><a class="postLink" href="';
-            item += './post.html?name=' + posts[i].name + '">' + posts[i].title + '</a>' + '<p class="date_and_category">';
-            item += posts[i].date + category + '</p><p class="description">' + posts[i].description + '</p></div>';
+
+            var item = '<div class="post-item">' +
+                '           <a class="postLink" href="./post.html?name=' + posts[i].name + '">' +
+                posts[i].title +
+                '           </a>' +
+                '           <p class="date_and_category">' +
+                posts[i].date +
+                '               <a href="./category.html?category=' + posts[i].category + '">' +
+                getCategoryTextByName(posts[i].category) +
+                '               </a>' +
+                '           </p>' +
+                '           <a href="post.html?name=' + posts[i].name + '">' +
+                '               <p class="description">' +
+                posts[i].description +
+                '               </p>' +
+                '           </a>' +
+                '       </div>';
+
             $("#main").append(item);
         }
     }
 
 }
 
-//todo: 获取导航条
-function getNavBar() {
+/**
+ * Print post list with specific category
+ *
+ * @param posts Posts to be printed
+ * @param category Category name
+ */
+function printCategoryUnit(posts, category) {
+
+    if (posts.length === 0) {
+        new RedirectTo404();
+    } else {
+        var categoryUnit = '<div class="categoryUnit">' +
+            '                   <h1 class="categoryTitle">' +
+            '                       /* class ' + category + ' */' +
+            '                   </h1>';
+        for (var i = 0; i < posts.length; i++) {
+            categoryUnit += '   <div class="categoryPostUnit">' +
+                '                   <p class="categoryDate">' +
+                posts[i].date +
+                '                   </p>';
+            categoryUnit += '       <a href="./post.html?name=' + posts[i].name + '" class="categoryLink">' +
+                posts[i].title + '' +
+                '                   </a>' +
+                '               </div>';
+        }
+        categoryUnit += '   </div>';
+
+        $("#post-list").append(categoryUnit);
+    }
 
 }
 
-//todo: 返回顶部
-function returnToTop() {
+/**
+ * Get navigation bar on page
+ */
+function getNavBar() {
+
+    var container = '<div class="container">' +
+        '                <div id="icon">' +
+        '                    <a href="./index.html">' +
+        '                        <img id="avatar" src="img/icon.jpg">' +
+        '                    </a>' +
+        '                </div>' +
+        '                <div id="list">' +
+        '                    <ul class="nav-list">';
+    for (var i = 0; i < navPages.length; i++) {
+        container += '           <li class="nav-list-item"><a href="' + navPages[i].url + '">' + navPages[i].name + '</a></li>';
+    }
+    container += '           </ul>' +
+        '                </div>' +
+        '           </div>';
+
+    $("#nav").append(container);
+
+}
+
+/**
+ * Sort by something
+ *
+ * @param name Element attribute
+ * @returns {Function} Sorting algorithm
+ */
+var by = function (name) {
+    return function (o, p) {
+        var a, b;
+        if (typeof o === "object" && typeof p === "object" && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+                return 0;
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1;
+            }
+            return typeof a < typeof b ? -1 : 1;
+        }
+        else {
+            throw ("error");
+        }
+    }
+};
+
+/**
+ * Get footer
+ */
+function getFooter() {
+
+    var footerText = '';
+    footerText += '<div id="footer">' +
+        '              <p>Copyright © 2014 - 2017 stormlin.</p>' +
+        '              <p>All Rights Reserved.</p>';
+    footerText += '    <p>' +
+        '                  <a href="http://www.miitbeian.gov.cn">备案号：粤ICP备16029958号-1</a>' +
+        '              </p>' +
+        '          </div>';
+
+    $("body").append(footerText);
+
+}
+
+/**
+ * Adjust div#main to make the page height at least one page height
+ */
+function adjustMainHeight() {
+
+    if (window.innerHeight) {
+        var windowHeight = window.innerHeight;
+    }
+    else if ((document.body) && (document.body.clientHeight)) {
+        var winHeight = document.body.clientHeight;
+    }
+    var main = $("#main");
+    var outerHeight = main.outerHeight(true);
+    var innerHeight = main.height();
+    var rem = parseInt(window.getComputedStyle(document.documentElement)["fontSize"]);
+    var title = $("#blog-info");
+    if (title === null) {
+        alert("null");
+        if ((outerHeight + 3 * rem) < windowHeight) {
+            alert("in");
+            var sub = windowHeight - (outerHeight + $("#footer").outerHeight());
+            main.height(innerHeight + sub);
+        }
+    } else {
+        alert("not null");
+        alert(outerHeight);
+        if ((outerHeight + title.outerHeight()) < windowHeight) {
+            alert("in");
+            alert("windowHeight: " + windowHeight);
+            alert("outerHeight: " + outerHeight);
+            alert("footerHeight: " + $("#footer").outerHeight());
+            var sub = windowHeight - (outerHeight + $("#footer").outerHeight());
+            alert("innerheight: " + innerHeight);
+            alert("sub: " + sub);
+            alert(innerHeight + sub);
+            main.height(innerHeight + sub);
+        }
+    }
+
+}
+
+/**
+ *
+ */
+function printPostAndAdjustHeight() {
+
+
+    var postName = getParaValue("name");
+    //获取文章信息
+    var post = getPostByName(postName);
+    //渲染文章标题
+    $("#title").html("Hello world!");
+    //获取并渲染写入文章正文
+    $.get("./md/" + postName + ".md").success(function (content) {
+        $(".markdown-body").append(marked(content));
+        $("pre").addClass("prettyprint linenums");
+        $("title").html($("h1").html());
+
+        adjustMainHeight();
+    }).error(RedirectTo404);
 
 }
